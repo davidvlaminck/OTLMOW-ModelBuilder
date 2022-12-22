0dff642c987f2af65ab10b37415c3e6f9ad2171d
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2022-12-22 15:48:02 +0100

    2.6 update

--- otlmow_modelbuilder/main.py
+++ otlmow_modelbuilder/main.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(message)s')
 
     current_dir = Path(__file__).parent
-    otl_subset_path = Path(current_dir / 'InputFiles' / 'OTL 2.5.db')
-    GA_file_path = Path(current_dir / 'InputFiles' / 'Geometrie_Artefact_2.5.db')
+    otl_subset_path = Path(current_dir / 'InputFiles' / 'OTL 2.6.db')
+    GA_file_path = Path(current_dir / 'InputFiles' / 'Geometrie_Artefact_2.6.db')
     model_directory = Path(current_dir.parent.parent / 'OTLMOW-Model/otlmow_model')
 
     ModelBuilder.build_otl_datamodel(otl_subset_location=otl_subset_path,
