feb83f657f189a9f12e4bb8c8cc74342ba4c2780
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-03-23 23:42:12 +0100

    refactor tests

--- UnitTests/OTLComplexDatatypeCreator_test.py
+++ UnitTests/OTLComplexDatatypeCreator_test.py
@@ -1,12 +1,13 @@
 import os
-import unittest
 from pathlib import Path
 from unittest.mock import MagicMock
 
-from otlmow_modelbuilder.SQLDataClasses.OSLOCollector import OSLOCollector
-from otlmow_modelbuilder.SQLDataClasses.OSLODatatypeComplex import OSLODatatypeComplex
+import pytest
+
 from otlmow_modelbuilder.OSLOInMemoryCreator import OSLOInMemoryCreator
 from otlmow_modelbuilder.OTLComplexDatatypeCreator import OTLComplexDatatypeCreator
+from otlmow_modelbuilder.SQLDataClasses.OSLOCollector import OSLOCollector
+from otlmow_modelbuilder.SQLDataClasses.OSLODatatypeComplex import OSLODatatypeComplex
 from otlmow_modelbuilder.SQLDbReader import SQLDbReader
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -68,67 +69,72 @@ expectedDtc = ["# coding=utf-8",
                "        return ComplexField.__str__(self)",
                ""]
 
+complex_datatype_validation_rules = {'valid_uri_and_types': {},
+                                     'valid_regexes': ["^https://wegenenverkeer.data.vlaanderen.be/ns/.+#Dtc.+"]}
 
-class OTLComplexDatatypeCreatorTests(unittest.TestCase):
-    complex_datatype_validation_rules = {'valid_uri_and_types': {},
-                                         'valid_regexes': ["^https://wegenenverkeer.data.vlaanderen.be/ns/.+#Dtc.+"]}
 
-    def test_InvalidOSLODatatypeComplexEmptyUri(self):
-        collector = OSLOCollector(MagicMock(spec=OSLOInMemoryCreator))
-        creator = OTLComplexDatatypeCreator(collector)
-        osloDatatypeComplex = OSLODatatypeComplex(name='name', objectUri='', definition='', label='', usagenote='',
-                                                  deprecated_version='')
+def test_InvalidOSLODatatypeComplexEmptyUri():
+    collector = OSLOCollector(MagicMock(spec=OSLOInMemoryCreator))
+    creator = OTLComplexDatatypeCreator(collector)
+    oslo_datatype_complex = OSLODatatypeComplex(name='name', objectUri='', definition='', label='', usagenote='',
+                                                deprecated_version='')
 
-        with self.assertRaises(ValueError) as exception_empty_uri:
-            creator.create_block_to_write_from_complex_types(
-                osloDatatypeComplex, complex_datatype_validation_rules=self.complex_datatype_validation_rules)
-        self.assertEqual(str(exception_empty_uri.exception), "OSLODatatypeComplex.objectUri is invalid. Value = ''")
+    with pytest.raises(ValueError) as exception_empty_uri:
+        creator.create_block_to_write_from_complex_types(
+            oslo_datatype_complex, complex_datatype_validation_rules=complex_datatype_validation_rules)
+    assert str(exception_empty_uri.value) == "OSLODatatypeComplex.objectUri is invalid. Value = ''"
 
-    def test_InvalidOSLODatatypeComplexBadUri(self):
-        collector = OSLOCollector(MagicMock(spec=OSLOInMemoryCreator))
-        creator = OTLComplexDatatypeCreator(collector)
-        osloDatatypeComplex = OSLODatatypeComplex(name='name', objectUri='Bad objectUri', definition='', label='',
-                                                  usagenote='', deprecated_version='')
 
-        with self.assertRaises(ValueError) as exception_bad_uri:
-            creator.create_block_to_write_from_complex_types(
-                osloDatatypeComplex, complex_datatype_validation_rules=self.complex_datatype_validation_rules)
-        self.assertEqual(str(exception_bad_uri.exception), "OSLODatatypeComplex.objectUri is invalid. Value = 'Bad objectUri'")
+def test_InvalidOSLODatatypeComplexBadUri():
+    collector = OSLOCollector(MagicMock(spec=OSLOInMemoryCreator))
+    creator = OTLComplexDatatypeCreator(collector)
+    oslo_datatype_complex = OSLODatatypeComplex(name='name', objectUri='Bad objectUri', definition='', label='',
+                                                usagenote='', deprecated_version='')
 
-    def test_InvalidOSLODatatypeComplexEmptyName(self):
-        collector = OSLOCollector(MagicMock(spec=OSLOInMemoryCreator))
-        creator = OTLComplexDatatypeCreator(collector)
-        osloDatatypeComplex = OSLODatatypeComplex(name='',
-                                                  objectUri='https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#DtcIdentificator',
-                                                  definition='', label='', usagenote='', deprecated_version='')
+    with pytest.raises(ValueError) as exception_bad_uri:
+        creator.create_block_to_write_from_complex_types(
+            oslo_datatype_complex, complex_datatype_validation_rules=complex_datatype_validation_rules)
+    assert str(exception_bad_uri.value) == "OSLODatatypeComplex.objectUri is invalid. Value = 'Bad objectUri'"
 
-        with self.assertRaises(ValueError) as exception_bad_name:
-            creator.create_block_to_write_from_complex_types(
-                osloDatatypeComplex, complex_datatype_validation_rules=self.complex_datatype_validation_rules)
-        self.assertEqual("OSLODatatypeComplex.name is invalid. Value = ''", str(exception_bad_name.exception))
 
-    def test_InValidType(self):
-        bad_complex = True
-        collector = OSLOCollector(MagicMock(spec=OSLOInMemoryCreator))
-        creator = OTLComplexDatatypeCreator(collector)
-        with self.assertRaises(ValueError) as exception_bad_name:
-            creator.create_block_to_write_from_complex_types(
-                bad_complex, complex_datatype_validation_rules=self.complex_datatype_validation_rules)
-        self.assertEqual(str(exception_bad_name.exception), "Input is not a OSLODatatypeComplex")
+def test_InvalidOSLODatatypeComplexEmptyName():
+    collector = OSLOCollector(MagicMock(spec=OSLOInMemoryCreator))
+    creator = OTLComplexDatatypeCreator(collector)
+    oslo_datatype_complex = OSLODatatypeComplex(
+        name='', definition='', label='', usagenote='', deprecated_version='',
+        objectUri='https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#DtcIdentificator')
 
-    def setUp(self) -> OSLOCollector:
-        base_dir = os.path.dirname(os.path.realpath(__file__))
-        file_location = Path(f'{base_dir}/OTL_AllCasesTestClass.db')
-        sql_reader = SQLDbReader(file_location)
-        oslo_creator = OSLOInMemoryCreator(sql_reader)
-        collector = OSLOCollector(oslo_creator)
-        collector.collect()
-        return collector
+    with pytest.raises(ValueError) as exception_bad_name:
+        creator.create_block_to_write_from_complex_types(
+            oslo_datatype_complex, complex_datatype_validation_rules=complex_datatype_validation_rules)
+    assert str(exception_bad_name.value) == "OSLODatatypeComplex.name is invalid. Value = ''"
 
-    def test_create_block_dtc(self):
-        collector = self.setUp()
-        creator = OTLComplexDatatypeCreator(collector)
-        datatype_complex = collector.find_complex_datatype_by_uri(
-            'https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#DtcIdentificator')
-        data_to_write = creator.create_block_to_write_from_complex_primitive_or_union_types(datatype_complex, type_field='Complex')
-        self.assertEqual(expectedDtc, data_to_write)
+
+def test_InValidType():
+    bad_complex = True
+    collector = OSLOCollector(MagicMock(spec=OSLOInMemoryCreator))
+    creator = OTLComplexDatatypeCreator(collector)
+    with pytest.raises(ValueError) as exception_bad_name:
+        creator.create_block_to_write_from_complex_types(
+            bad_complex, complex_datatype_validation_rules=complex_datatype_validation_rules)
+    assert str(exception_bad_name.value) == "Input is not a OSLODatatypeComplex"
+
+
+def setUp() -> OSLOCollector:
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    file_location = Path(f'{base_dir}/OTL_AllCasesTestClass.db')
+    sql_reader = SQLDbReader(file_location)
+    oslo_creator = OSLOInMemoryCreator(sql_reader)
+    collector = OSLOCollector(oslo_creator)
+    collector.collect()
+    return collector
+
+
+def test_create_block_dtc():
+    collector = setUp()
+    creator = OTLComplexDatatypeCreator(collector)
+    datatype_complex = collector.find_complex_datatype_by_uri(
+        'https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#DtcIdentificator')
+    data_to_write = creator.create_block_to_write_from_complex_primitive_or_union_types(
+        datatype_complex, type_field='Complex')
+    assert data_to_write == expectedDtc
