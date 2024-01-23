6cfd237c38c23d9ec8e6ae917373df35b5d8d95a
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-01-23 10:19:35 +0100

    test model now uses a subset without list in list

--- UnitTests/CreateAllTestCases_test.py
+++ UnitTests/CreateAllTestCases_test.py
@@ -15,19 +15,31 @@ def test_init_AllCasesTestClass_using_modelbuilder(subtests, caplog):
     otl_subset_location = Path(f'{base_dir}/OTL_AllCasesTestClass.db')
     ga_location = Path(f'{base_dir}/GeometrieArtefact_AllCasesTestClass.db')
 
-
-
     paths_to_create_test_class = [Path(f'{base_dir}/TestModel'),
                                   Path(f'{base_dir}/../../OTLMOW-Model/UnitTests/TestModel'),
-                                  # Path(f'{base_dir}/../../OTLMOW-Converter/UnitTests/TestModel'),
-                                  # Path(f'{base_dir}/../../OTLMOW-Template/UnitTests/TestModel')
-                                  ]
+                                  Path(f'{base_dir}/../../OTLMOW-Converter/UnitTests/TestModel'),
+                                  Path(f'{base_dir}/../../OTLMOW-Template/UnitTests/TestModel')]
 
     for path in paths_to_create_test_class:
         with subtests.test(msg=f'Creating testclasses for {path}'):
             with warnings.catch_warnings():
                 warnings.filterwarnings("ignore", category=DeprecationWarning)  # supress deprecation warnings from tqdm
                 ModelBuilder.build_otl_datamodel(otl_subset_location=otl_subset_location,
+                                                 geometry_artefact_location=ga_location,
+                                                 directory=path)
+
+        all_cases_class_location = Path(path / 'OtlmowModel/Classes/Onderdeel/AllCasesTestClass.py')
+        assert os.path.isfile(all_cases_class_location)
+
+
+    otl_subset_location_no_double_kard = Path(f'{base_dir}/OTL_AllCasesTestClass_no_double_kard.db')
+    paths_to_create_test_class_no_double_kard = [Path(f'{base_dir}/../../OTLMOW-Template/UnitTests/TestModel')]
+
+    for path in paths_to_create_test_class_no_double_kard:
+        with subtests.test(msg=f'Creating testclasses for {path}'):
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore", category=DeprecationWarning)  # supress deprecation warnings from tqdm
+                ModelBuilder.build_otl_datamodel(otl_subset_location=otl_subset_location_no_double_kard,
                                                  geometry_artefact_location=ga_location,
                                                  directory=path)
 
