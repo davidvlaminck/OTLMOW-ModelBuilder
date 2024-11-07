7bb1a8d0e363cb6c5dd16476a137d55d7b908c9d
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-11-07 23:12:07 +0100

    moved Inputfiles

--- otlmow_modelbuilder/main.py
+++ otlmow_modelbuilder/main.py
@@ -9,8 +9,8 @@ def create_model():
     logging.basicConfig(level=logging.INFO, format='%(message)s')
 
     current_dir = Path(__file__).parent
-    otl_subset_path = Path(current_dir / 'InputFiles' / 'latest' / 'OTL.db')
-    GA_file_path = Path(current_dir / 'InputFiles' / 'latest' / 'Geometrie_Artefact.db')
+    otl_subset_path = Path(current_dir.parent / 'InputFiles' / 'latest' / 'OTL.db')
+    GA_file_path = Path(current_dir.parent / 'InputFiles' / 'latest' / 'Geometrie_Artefact.db')
     model_directory = Path(current_dir.parent.parent / 'OTLMOW-Model/otlmow_model')
 
     ModelBuilder.build_otl_datamodel(otl_subset_location=otl_subset_path, geometry_artefact_location=GA_file_path,
