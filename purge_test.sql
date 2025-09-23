115c5dcfc1d84dcd6a10d9e352abd27f271dc475
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2025-09-23 15:11:03 +0200

    sql queries to remove "-test" in uri's

---
+++ purge_test.sql
@@ -0,0 +1,75 @@
+UPDATE InternalBaseClass
+SET class_uri = REPLACE(class_uri, '-test', '')
+WHERE class_uri LIKE '%-test%';
+
+UPDATE InternalBaseClass
+SET base_uri = REPLACE(base_uri, '-test', '')
+WHERE base_uri LIKE '%-test%';
+
+UPDATE OSLOClass
+SET uri = REPLACE(uri, '-test', '')
+WHERE uri LIKE '%-test%';
+
+UPDATE OSLOAttributen
+SET uri = REPLACE(uri, '-test', '')
+WHERE uri LIKE '%-test%';
+
+UPDATE OSLOAttributen
+SET class_uri = REPLACE(class_uri, '-test', '')
+WHERE class_uri LIKE '%-test%';
+
+UPDATE OSLODatatypeComplex
+SET uri = REPLACE(uri, '-test', '')
+WHERE uri LIKE '%-test%';
+
+UPDATE OSLODatatypeComplexAttributen
+SET uri = REPLACE(uri, '-test', '')
+WHERE uri LIKE '%-test%';
+
+UPDATE OSLODatatypeComplexAttributen
+SET class_uri = REPLACE(class_uri, '-test', '')
+WHERE class_uri LIKE '%-test%';
+
+UPDATE OSLODatatypePrimitive
+SET uri = REPLACE(uri, '-test', '')
+WHERE uri LIKE '%-test%';
+
+UPDATE OSLODatatypePrimitiveAttributen
+SET uri = REPLACE(uri, '-test', '')
+WHERE uri LIKE '%-test%';
+
+UPDATE OSLODatatypePrimitiveAttributen
+SET class_uri = REPLACE(class_uri, '-test', '')
+WHERE class_uri LIKE '%-test%';
+
+UPDATE OSLODatatypeUnion
+SET uri = REPLACE(uri, '-test', '')
+WHERE uri LIKE '%-test%';
+
+UPDATE OSLODatatypeUnionAttributen
+SET uri = REPLACE(uri, '-test', '')
+WHERE uri LIKE '%-test%';
+
+UPDATE OSLODatatypeUnionAttributen
+SET class_uri = REPLACE(class_uri, '-test', '')
+WHERE class_uri LIKE '%-test%';
+
+UPDATE OSLOEnumeration
+SET uri = REPLACE(uri, '-test', '')
+WHERE uri LIKE '%-test%';
+
+UPDATE OSLORelaties
+SET uri = REPLACE(uri, '-test', '')
+WHERE uri LIKE '%-test%';
+
+UPDATE OSLORelaties
+SET bron_uri = REPLACE(bron_uri, '-test', '')
+WHERE bron_uri LIKE '%-test%';
+
+UPDATE OSLORelaties
+SET doel_uri = REPLACE(doel_uri, '-test', '')
+WHERE doel_uri LIKE '%-test%';
+
+UPDATE TypeLinkTabel
+SET item_uri = REPLACE(item_uri, '-test', '')
+WHERE item_uri LIKE '%-test%';
